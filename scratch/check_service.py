53d52186cd4fbca0d627187816a2ba8517b6a33e
Willy Gallardo <wgallardo@powerlink.local>
2026-09-18 07:32:01 -0400

    chore: add dynamic files and scratch to gitignore and remove them from index

--- scratch/check_service.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect('10.0.1.179', username='root', password='Redes2010')
-
-stdin, stdout, stderr = client.exec_command('systemctl cat intranet_qa')
-print('STDOUT:\n', stdout.read().decode())
-print('STDERR:\n', stderr.read().decode())
